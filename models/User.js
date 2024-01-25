40c2824f229d7ec41bcf7a1b16300cf077d80f50
kennycummings <kennethccummings@gmail.com>
2024-01-25 14:27:13 -0500

    Wrote out models .js code

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, required: true, unique: true, match: /.+@.+\..+/ },
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+// Virtual for friendCount
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
